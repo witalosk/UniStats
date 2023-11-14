6dda3dc57d62d44e879d0e21ec2ac93ac4bd7bb6
witalosk <shun_september@live.jp>
2023-11-14 22:38:57 +0900

    [add] RenderingModule

---
+++ Packages/com.witalosk.unistats/Runtime/Modules/RenderingDataProvider.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Profiling;
+using UnityEngine;
+
+namespace UniStats
+{
+    public class RenderingDataProvider : MonoBehaviour, ITripleDataProvider
+    {
+        public float Element1Value => _drawCallsRecorder.LastValue;
+        public float Element2Value => _setPassCallsRecorder.LastValue;
+        public float Element3Value => _verticesRecorder.LastValue / 1000f;
+        
+        private ProfilerRecorder _drawCallsRecorder;
+        private ProfilerRecorder _setPassCallsRecorder;
+        private ProfilerRecorder _verticesRecorder;
+
+        private void Start()
+        {
+            _drawCallsRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Render, "Draw Calls Count");
+            _setPassCallsRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Render, "SetPass Calls Count");
+            _verticesRecorder = ProfilerRecorder.StartNew(ProfilerCategory.Render, "Vertices Count");
+        }
+
+        private void OnDestroy()
+        {
+            _drawCallsRecorder.Dispose();
+            _setPassCallsRecorder.Dispose();
+            _verticesRecorder.Dispose();
+        }
+    }
+}
